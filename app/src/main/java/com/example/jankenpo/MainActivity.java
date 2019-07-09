b45c6e62669eaec9362475c34fd26667f43f050b
JhouAvila <jhonatajhou@gmail.com>
2019-07-09 15:33:36 -0300

    - Layout problems; - The animation "desappear" don't run at the second time.

--- app/src/main/java/com/example/jankenpo/MainActivity.java
+++ app/src/main/java/com/example/jankenpo/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.jankenpo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     ViewHolder mViewHolder = new ViewHolder();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,48 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.paper = findViewById(R.id.buttonPaper);
         this.mViewHolder.rock = findViewById(R.id.buttonRock);
         this.mViewHolder.scissors = findViewById(R.id.buttonScissors);
+        this.mViewHolder.appear = new AlphaAnimation(0,1);
+        this.mViewHolder.desappear = new AlphaAnimation(1,0);
+        this.mViewHolder.desappear.setDuration(100);
+        this.mViewHolder.appear.setDuration(1500);
+
+        this.mViewHolder.desappear.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                mViewHolder.player2.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mViewHolder.player2.setVisibility(View.INVISIBLE);
+                mViewHolder.player2.setAnimation(mViewHolder.appear);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        this.mViewHolder.appear.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                mViewHolder.player2.setVisibility(View.INVISIBLE);
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mViewHolder.player2.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
     public void tocouBotao(View view){
 
@@ -30,16 +74,21 @@ public class MainActivity extends AppCompatActivity {
 
             case(R.id.buttonRock): this.mViewHolder.player1.setImageResource(R.drawable.pedra);
             break;
+
             case (R.id.buttonScissors): this.mViewHolder.player1.setImageResource(R.drawable.tesoura);
             break;
 
 
         }
         this.mViewHolder.player2.setImageResource(R.drawable.interrogacao);
+        this.mViewHolder.player2.setAnimation(mViewHolder.desappear);
 
     }
     public static class ViewHolder{
         ImageView player1,player2;
         ImageButton rock, paper, scissors;
+        Animation appear, desappear;
+
     }
+
 }
